95a2b2674a97b4e92173d59e48cfeb01ae655b50
askl56 <amscott@openmailbox.org>
2016-07-28 11:49:50 +0100

    Update base currency to GBP

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -2,6 +2,6 @@ module Spree
   AppConfiguration.class_eval do
     preference :allow_currency_change,  :boolean, default: false
     preference :show_currency_selector, :boolean, default: false
-    preference :supported_currencies,   :string,  default: 'USD'
+    preference :supported_currencies,   :string,  default: 'GBP'
   end
 end
